dfef00361b5b1dc28ad13c09ee836101cd9aaa47
GiorgioMartini <jorgeantonio82@googlemail.com>
2018-12-04 10:01:26 +0100

    added mongoose conection to save datat to db

--- maketogether/routes/index.js
+++ maketogether/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const storeRouter = require('../controllers/storeController')
+const { catchErrors } = require('../handlers/errorHandlers')
+
 
 // Hit this specif route '/' then do something
 router.get('/', storeRouter.homePage)
-
 // Hit /add then do something
 router.get('/add', storeRouter.addStore)
+// Hit /add then do something
+router.post('/add', catchErrors(storeRouter.createStore))
 
 module.exports = router
